5db6bcbcd6e36be3c2cb0bb3e97a4c737a7a6f74
cmdr-nova <cmdr-nova@proton.me>
2024-11-04 22:59:14 -0500

    updating to use proxy

--- assets/js/tumblr.js
+++ assets/js/tumblr.js
@@ -1,9 +1,9 @@
-const apiKey = 'fqSGD7oySzZojmigIu6irCjfWX3sz4jjRNvXVmp92jyVfoApIf';
 const blogIdentifier = 'nova-ayashi.tumblr.com';
+const proxyUrl = 'http://67.205.188.225:3000/tumblr'; // Replace with your VPS domain or IP and port
 
 async function fetchTumblrPosts() {
   try {
-    const response = await fetch(`https://api.tumblr.com/v2/blog/${blogIdentifier}/posts?api_key=${apiKey}`);
+    const response = await fetch(`${proxyUrl}?blogIdentifier=${blogIdentifier}`);
     const data = await response.json();
     const posts = data.response.posts;
     displayTumblrPosts(posts);
