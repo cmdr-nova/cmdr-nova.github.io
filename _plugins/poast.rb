016034c6d83398d193fe4ebd5af4dc1241cb42d8
cmdr-nova <cmdr-nova@proton.me>
2024-11-07 01:04:29 -0500

    removing post, updating ruby

--- _plugins/poast.rb
+++ _plugins/poast.rb
@@ -1,5 +1,5 @@
 Jekyll::Hooks.register :posts, :post_write do |post|
     post_content = "New post published: #{post.data['title']} - #{post.url}"
     escaped_post_content = Shellwords.escape(post_content)
-    system("ssh root@67.205.188.225 'python3 masto-poast/poast.py #{escaped_post_content}'")
+    system("ssh root@67.205.188.225 'python3 ~/masto-poast/poast.py #{escaped_post_content}'")
   end
