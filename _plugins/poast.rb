20f4f66fca97280fae74fd1f252251266187fe02
cmdr-nova <cmdr-nova@proton.me>
2024-11-07 01:06:37 -0500

    updating ruby and yml

--- _plugins/poast.rb
+++ _plugins/poast.rb
@@ -1,5 +1,9 @@
+require 'shellwords'
+
 Jekyll::Hooks.register :posts, :post_write do |post|
-    post_content = "New post published: #{post.data['title']} - #{post.url}"
-    escaped_post_content = Shellwords.escape(post_content)
-    system("ssh root@67.205.188.225 'python3 ~/masto-poast/poast.py #{escaped_post_content}'")
-  end
+  post_content = "New post published: #{post.data['title']} - #{post.url}"
+  escaped_post_content = Shellwords.escape(post_content)
+  puts "Post content: #{post_content}"
+  puts "Escaped post content: #{escaped_post_content}"
+  system("ssh root@67.205.188.225 'python3 masto-poast/poast.py #{escaped_post_content}'")
+end
