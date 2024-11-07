1339624625b8fcf448cbe67a04232eb536db1710
cmdr-nova <cmdr-nova@proton.me>
2024-11-07 07:08:28 -0500

    updating the ruby

--- _plugins/poast.rb
+++ _plugins/poast.rb
@@ -1,9 +1,22 @@
 require 'shellwords'
+require 'nokogiri'
 
 Jekyll::Hooks.register :posts, :post_write do |post|
-  post_content = "New post published: #{post.data['title']} - #{post.url}"
+  # Extract the first paragraph as a summary
+  doc = Nokogiri::HTML(post.content)
+  summary = doc.css('p').first.text
+
+  # Extract the tag from the front matter
+  tag = post.data['tag']
+  hashtag = "##{tag.gsub(' ', '').downcase}"
+
+  # Construct the post content
+  post_content = "New post published: #{post.data['title']} - #{post.url}\n\n#{summary}\n\n#{hashtag}"
   escaped_post_content = Shellwords.escape(post_content)
+
   puts "Post content: #{post_content}"
   puts "Escaped post content: #{escaped_post_content}"
+
+  # Execute the SSH command to run the Python script
   system("ssh root@67.205.188.225 \"python3 masto-poast/poast.py #{escaped_post_content}\"")
 end
