061606fea5571ee22ece94c26ce5e679c814b18a
cmdr-nova <cmdr-nova@proton.me>
2024-11-15 21:04:46 -0500

    change to being consistent

---
+++ assets/js/initial-state.js
@@ -0,0 +1,4 @@
+// Check the stored preference and apply it immediately
+if (localStorage.getItem('lightMode') === 'enabled') {
+    document.body.classList.add('light-mode');
+  }
--- assets/js/light-mode.js
+++ assets/js/light-mode.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleSwitch = document.getElementById('light-mode-toggle');
-  
-    toggleSwitch.addEventListener('change', () => {
-      document.body.classList.toggle('light-mode', toggleSwitch.checked);
-    });
+  const toggleSwitch = document.getElementById('light-mode-toggle');
+
+  // Check the stored preference and apply it
+  const currentMode = localStorage.getItem('lightMode');
+  if (currentMode === 'enabled') {
+    document.body.classList.add('light-mode');
+    toggleSwitch.checked = true;
+  }
+
+  toggleSwitch.addEventListener('change', () => {
+    if (toggleSwitch.checked) {
+      document.body.classList.add('light-mode');
+      localStorage.setItem('lightMode', 'enabled');
+    } else {
+      document.body.classList.remove('light-mode');
+      localStorage.setItem('lightMode', 'disabled');
+    }
   });
+});
